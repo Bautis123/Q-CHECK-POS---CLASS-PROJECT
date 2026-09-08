14b5151eab74dbef23b50117bd98ed311dbad375
Sereni Banda <serenijoelbanda12@gmail.com>
2026-09-08 09:40:57 +0200

    Added images to respective prodcts

--- backend/config/Database.php
+++ backend/config/Database.php
@@ -5,7 +5,7 @@ final class Database
     private const HOST = '127.0.0.1';
     private const DB_NAME = 'gadget_pos';
     private const USER = 'root';
-    private const PASSWORD = '';
+    private const PASSWORD = 'rootsung';
 
     public static function connect(): PDO
     {
